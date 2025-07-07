615d4e2bee5fe5f8bebfc5c04b61a79b4dc16505
KycaB4ik <biterhurt@gmail.com>
2025-07-08 01:52:26 +0200

    Create stats.js

---
+++ pages/api/stats.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  const url = 'https://gsx2json.com/api?id=1ohEkz-AFToTFeiP6q4uZP_JUZtBgxToiBuNVlpk7wL0&sheet=Лист%201';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+}
